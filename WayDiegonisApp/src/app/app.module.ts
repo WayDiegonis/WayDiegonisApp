bd197c4d0b979e077c66fac0cbf70fecf71e731e
root <diegoneveslourenco@gmail.com>
2019-09-09 20:25:31 -0300

    API (Colocação)

--- WayDiegonisApp/src/app/app.module.ts
+++ WayDiegonisApp/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+imnport {HttpClientModule} from '@angular/common/http';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
